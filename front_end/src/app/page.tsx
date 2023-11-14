d2e7cf06c3181d43852195e0e474fd8d83899412
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-11-14 07:34:31 +0200

    Revert "git commit -m SG-42 sign up form - incomplete"
    This reverts commit d5668c6e4b37261da654b6a57688dfcecf77b4a3.

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -1,14 +1,15 @@
+import Image from 'next/image'
 import styles from './page.module.css'
-import Link from "next/link";
-import Login from "./login/page";
-import {createTheme} from "@mui/material";
+import Button from '@mui/material/Button';
 
-
-
-function Home() {
+export default function Home() {
   return (
-        <Login />
+    <div>
+        <p className={styles.paragraph}> Hello greys </p>
+        {/*testing mui
+        installation -> npm install @mui/material @emotion/react @emotion/styled
+        documentation -> https://mui.com/material-ui/getting-started/installation/*/  }
+        <Button className={styles.main_button} size="large" variant="contained">press me</Button>
+    </div>
   )
 }
-export default Home
-
