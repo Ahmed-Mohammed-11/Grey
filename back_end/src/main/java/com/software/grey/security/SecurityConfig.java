6586f1ecd750a65c8b80bd93ec5424504d3cd19e
Osama Belal <obelal010@gmail.com>
2023-11-16 11:06:55 +0200

    SG-103 Google OAuth working with no redirect

--- back_end/src/main/java/com/software/grey/security/SecurityConfig.java
+++ back_end/src/main/java/com/software/grey/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,7 @@ import static com.software.grey.utils.EndPoints.SIGNUP;
 import static com.software.grey.utils.EndPoints.TEST;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -31,6 +33,7 @@ public class SecurityConfig {
         userDetailsManager.setAuthoritiesByUsernameQuery("SELECT username, role FROM user WHERE username=?");
         return userDetailsManager;
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -53,5 +56,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
--- front_end/src/app/signup/Controllers/userController.ts
+++ front_end/src/app/signup/Controllers/userController.ts
@@ -19,30 +19,3 @@ class UserController {
 }
 
 export default new UserController();
-
-
-
-
-//
-// const url = 'http://localhost:8080';
-//
-// function handleRequests(userDTO: UserDTO) {
-//     const response = UserController(userDTO);
-// }
-//
-// function UserController (credentials: UserDTO) : any {
-//     const endpoint = url + '/signup';
-//     let headers = new Headers();
-//     headers.append('Content-Type', 'application/json');
-//
-//     fetch(endpoint, {
-//         method: 'POST',
-//         body: JSON.stringify(credentials),
-//         headers: headers
-//     })
-//         .then(response => console.log(response))
-//         .then(data => console.log("data", data))
-//         .catch(error => console.log('Authorization failed : ' + error.message));
-// }
-//
-// export default handleRequests;
