dce931d1734125ff9bda770c8c919ddb17d27e95
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-11-21 09:51:11 +0200

    SG-106 front end code refactored and additional files/folders added

---
+++ front_end/src/app/constants/displayTextConstants.tsx
@@ -0,0 +1,11 @@
+export const signupPanelText = 'Social media platform that provides anonymous\n' +
+    '                        experience for users to freely express their feelings and opinions, participate in events and\n' +
+    '                        much more.'
+
+
+export const signinPanelText = 'Welcome Back!'
+
+export const displayTextConstants = {
+    signupPanelText: signupPanelText,
+    signinPanelText: signinPanelText
+};
