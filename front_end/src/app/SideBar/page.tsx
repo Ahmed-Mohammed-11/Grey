bb2bfc41570480dbdbea7aaa18df94e630f78ab7
josephShokry <josephshokry9@gmail.com>
2023-11-29 10:03:22 +0200

    Add the dynamic feeds of the  font end

--- front_end/src/app/SideBar/page.tsx
+++ front_end/src/app/SideBar/page.tsx
@@ -1,38 +1,54 @@
+// SideBar.js
 import Button from '@mui/material/Button';
 import styles from "./page.module.css"
-import {Box} from "@mui/system";
-import {MdExplore} from "react-icons/md";
-import {AiFillHome} from "react-icons/ai";
-import {FaUserLarge} from "react-icons/fa6";
-import {RiSettings4Fill} from "react-icons/ri";
-import {BsBookmark, BsFillBookmarkFill} from "react-icons/bs";
+import { Box } from "@mui/system";
+import { MdExplore } from "react-icons/md";
+import { AiFillHome } from "react-icons/ai";
+import { FaUserLarge } from "react-icons/fa6";
+import { RiSettings4Fill } from "react-icons/ri";
+import { BsFillBookmarkFill } from "react-icons/bs";
+import { FaBookOpen } from "react-icons/fa";
 import Profile from "@/app/SideBar/profile";
+import { useState } from 'react';
 
+export default function SideBar(props:any) {
+  const [activeTab, setActiveTab] = useState(0);
 
-export default function SideBar(props: any) {
-    // TODO: change active tab index when clicked
-    var active_tab = 0
+  const buttonsText = ["feed", "explore", "diary", "saved", "profile", "settings"];
+  const buttonsIcons = [
+    <AiFillHome />,
+    <MdExplore />,
+    <FaBookOpen />,
+    <BsFillBookmarkFill />,
+    <FaUserLarge />,
+    <RiSettings4Fill />,
+  ];
 
-    const buttonsText = ["feed", "explore", "profile", "settings", "saved"]
-    const buttonsIcons = [
-        <AiFillHome/>,
-        <MdExplore/>,
-        <FaUserLarge/>,
-        <RiSettings4Fill/>,
-        <BsFillBookmarkFill/>]
-    const buttons = buttonsText.map((text, index) => {
-        return (
-            <Button className={`${styles.button} ${index === active_tab ? styles.active : styles.button}`} key={index}>
-                {buttonsIcons[index]}
-                {text}
-            </Button>
-        )
-    })
+  const handleButtonClick = (index:number) => {
+    setActiveTab(index);
+    // Call the onChange function passed from the parent
+    if (props.onChange) {
+      props.onChange(index);
+    }
+  };
 
+  const buttons = buttonsText.map((text, index) => {
     return (
-        <Box className={styles.side_bar} width={props.width}>
-            <Profile name={"@hesham09"}></Profile>
-            <Box className={styles.container_buttons}>{buttons}</Box>
-        </Box>
-    )
+      <Button
+        key={index}
+        className={`${styles.button} ${index === activeTab ? styles.active : styles.button}`}
+        onClick={() => handleButtonClick(index)}
+      >
+        {buttonsIcons[index]}
+        {text}
+      </Button>
+    );
+  });
+
+  return (
+    <Box className={styles.side_bar} width={props.width}>
+      <Profile name={"@hesham09"}></Profile>
+      <Box className={styles.container_buttons}>{buttons}</Box>
+    </Box>
+  );
 }
--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -1,15 +1,54 @@
-import Image from 'next/image'
+'use client'
+
 import styles from './page.module.css'
 import SideBar from "@/app/SideBar/page";
-import {Box} from "@mui/system";
+import { Box } from "@mui/system";
 import Feed from "@/app/Feed/page";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [selectedPageIndex, setSelectedPageIndex] = useState(0);
+  const [feedData, setFeedData] = useState(null);
+
+  const urlBase = 'http://localhost:8080'
+
+  const endpointMapping = {
+    0: '/posts/feed',
+    1: '/posts/explore',
+    2: '/posts/diary',
+    3: '/posts/saved',
+    4: '/posts/profile',
+    5: '/posts/settings',
+  };
+
+  const fetchData = async () => {
+    try {
+      const endpoint = endpointMapping[selectedPageIndex];
+      if (!endpoint) {
+        console.error('Invalid selectedPageIndex:', selectedPageIndex);
+        return;
+      }
+
+      const response = await fetch(urlBase + endpoint);
+      const data = await response.json();
+      setFeedData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [selectedPageIndex]);
+
+  const handleChange = (newSelectedPageIndex:number) => {
+    setSelectedPageIndex(newSelectedPageIndex);
+  };
 
   return (
-      <Box className={styles.container}>
-        <SideBar width={"25%"}/>
-        <Feed width={"75%"}/>
-      </Box>
-  )
+    <Box className={styles.container}>
+      <SideBar width={"25%"} onChange={handleChange} />
+      <Feed width={"75%"} data={feedData} />
+    </Box>
+  );
 }
