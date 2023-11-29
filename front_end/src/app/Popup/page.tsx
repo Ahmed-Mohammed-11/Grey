f6cc0544cdfb187a7a7d8ca7a76c13d8349562cb
Osama Belal <obelal010@gmail.com>
2023-11-29 13:38:18 +0200

    SG-29 Validated post and feeling insertion

--- front_end/src/app/Popup/page.tsx
+++ front_end/src/app/Popup/page.tsx
@@ -1,46 +1,81 @@
 "use client";
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import Popup from 'reactjs-popup';
 import styles from "./page.module.css"
 import Profile from "@/app/SideBar/profile";
 import {Box} from "@mui/system";
 import {FaPen} from "react-icons/fa";
 import {IoSend} from "react-icons/io5";
-import {Chip, ListItem, TextField} from "@mui/material";
+import {IoMdAdd} from "react-icons/io";
+import {Chip, IconButton, ListItem, Menu, MenuItem, TextareaAutosize, Tooltip} from "@mui/material";
 import Feeling from "@/app/DTO/Feeling";
 import PostDTO from "@/app/DTO/PostDTO";
 import postController from "@/app/services/postController";
 import {createPostEndPoint} from "@/app/constants/apiConstants";
+import Button from "@mui/material/Button";
+
+const allFeelings = new Set<Feeling>([Feeling.HAPPY, Feeling.SAD,
+    Feeling.ANGER, Feeling.DISGUST,
+    Feeling.FEAR, Feeling.ANXIOUS,
+    Feeling.INSPIRE, Feeling.LOVE]);
 
 export default function PopupScreen() {
-    const [feelings, setFeelings] =
-        React.useState(new Set<Feeling>([Feeling.HAPPY, Feeling.SAD, Feeling.ANGER, Feeling.DISGUST,
-            Feeling.FEAR, Feeling.ANXIOUS, Feeling.INSPIRE, Feeling.LOVE]));
 
+    // Feelings drop down menu
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClose = () => {setAnchorEl(null);};
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {setAnchorEl(event.currentTarget);};
+
+
+    // Feelings chips
+    const [selectedFeelings, setSelectedFeelings] = React.useState(new Set<Feeling>());
+    const [isFeelingsValid, setIsFeelingsValid] = React.useState(false);
+    const [fullFeelings, setFullFeelings] = React.useState(false);
+
+    const handleFeelingsChange = () => {
+        if(selectedFeelings.size === 0) setIsFeelingsValid(false)
+        else setIsFeelingsValid(true)
+
+        if (selectedFeelings.size < 3) setFullFeelings(false)
+        else setFullFeelings(true)
+    }
+    const handleAdd = (feeling: Feeling) => () => {
+        if (selectedFeelings.size < 3)
+            setSelectedFeelings((feelings) => {
+                feelings.add(feeling);
+                return new Set<Feeling>(feelings);
+            });
+        handleFeelingsChange();
+    };
     const handleDelete = (chipToDelete: Feeling) => () => {
-        setFeelings((feelings) => {
+        setSelectedFeelings((feelings) => {
             feelings.delete(chipToDelete);
             return new Set<Feeling>(feelings);
         });
+        handleFeelingsChange();
     };
 
-    const postText = useRef<HTMLInputElement>(null);
+
+    // Handling post text
+    const [isPostTextValid, setIsPostTextValid] = React.useState("");
+    const handlePostText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {setIsPostTextValid(event.target.value);}
     const handleCreatePost = () => {
         let post: PostDTO = {
-            postText: postText.current!.value,
-            postFeelings: feelings
+            postText: isPostTextValid,
+            postFeelings: selectedFeelings
         }
-
         console.log(post);
-        // post.postText && sendInfoToServer(post)
+        isFeelingsValid && isPostTextValid && sendInfoToServer(post)
     }
 
+
+    // send post to server
     async function sendInfoToServer(post: PostDTO) {
         let postDTO: PostDTO = {
             postText: post.postText,
             postFeelings: post.postFeelings
         }
-
         fetchResponse(postDTO);
     }
 
@@ -66,36 +101,68 @@ export default function PopupScreen() {
                             <div className={styles.profile}>
                                 <Profile name={"@hesham09"}/>
                             </div>
-                            <button className={styles.close} onClick={close}>
-                                &times;
-                            </button>
-                            <TextField
-                                className={styles.input}
-                                aria-valuemax={5000}
-                                placeholder={"Pour it out to the world"}
-                                inputRef={postText}></TextField>
 
-                            <Box className={styles.post}>
+                            <Box className={styles.feelings}>
                                 <ListItem>
-                                    {Array.from(feelings).map((feeling: any, feelingIndex: any) => (
+                                    {Array.from(selectedFeelings).map((feeling: any, feelingIndex: any) => (
                                         <Chip
-                                            // icon={icons[feelingIndex]}
                                             label={feeling}
                                             className={feeling}
                                             onDelete={handleDelete(feeling)}
                                             key={feelingIndex}/>
                                     ))}
                                 </ListItem>
+                                <Tooltip title="Add Feeling">
+                                    <IconButton
+                                        id="basic-button"
+                                        disabled={fullFeelings}
+                                        className={styles.feeling_button}
+                                        aria-controls={open ? 'basic-menu' : undefined}
+                                        aria-haspopup="true"
+                                        aria-expanded={open ? 'true' : undefined}
+                                        onClick={handleClick}>
+                                        <IoMdAdd/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    id="basic-menu"
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                    MenuListProps={{'aria-labelledby': 'basic-button',}}>
+                                    {Array.from(allFeelings).filter((feeling) =>
+                                        !selectedFeelings.has(feeling)).map((feeling: any, feelingIndex: any) => (
+                                        <MenuItem className={feeling} onClick={handleAdd(feeling)}>
+                                            {feeling}
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+
                             </Box>
 
+                            <TextareaAutosize
+                                className={styles.input}
+                                maxLength={5000}
+                                placeholder={"Pour it out to the world"}
+                                value={isPostTextValid}
+                                onChange={handlePostText}></TextareaAutosize>
+
                             <div className={styles.actions}>
-                                <button className={styles.button} onClick={() => close()}>
+                                <button className={styles.button} onClick={close}>
                                     cancel
                                 </button>
-                                <button className={`${styles.button} ${styles.filled}`} onClick={() => handleCreatePost()}>
+                                <button
+                                        className={`${styles.button} ${styles.filled}
+                                        ${!isFeelingsValid || !isPostTextValid ? styles.disabled : ""}`}
+                                        disabled={!isFeelingsValid || !isPostTextValid}
+                                        onClick={handleCreatePost}>
                                     create post <IoSend/>
                                 </button>
                             </div>
+
+                            <button className={styles.close} onClick={close}>
+                                &times;
+                            </button>
                         </div>
                     </section>
                 );
