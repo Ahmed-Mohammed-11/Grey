c6a5728cd26edfe22e56d1d62301ea7ae07b92cb
josephShokry <josephshokry9@gmail.com>
2023-12-06 15:43:05 +0200

    Add the scroll to the correct feed

--- front_end/src/app/components/posts/page.tsx
+++ front_end/src/app/components/posts/page.tsx
@@ -2,16 +2,85 @@ import styles from "./page.module.css"
 import {Box} from "@mui/system";
 import Post from "@/app/components/post/page";
 import Feeling from '../../models/dtos/Feeling';
+import React, { useState, useEffect } from 'react';
+import { useInView } from "react-intersection-observer"
+import { BASE_BACKEND_URL, DIARY_ENDPOINT } from "@/app/constants/apiConstants";
 
 export default function Feed(props:any) {
-    const posts = props.data || []
+    const {ref, inView } = useInView();
+    const [auth, setAuth] = useState<string | null>(null);
+    const [pageIndex, setPageIndex] = useState(-1);
+    const [pageSize, setPageSize] = useState(5);
+    const [totalNumberOfPages, setTotalNumberOfPages] = useState(10);
+    const [posts, setPosts] = useState<any[]>([]);
+    
+    useEffect(() => {
+        setAuth(localStorage.getItem('Authorization'));
+    }, []);
+    
+    useEffect(() => {
+        setPageIndex(0)
+        setPosts([])
+    }, [props.feedType]);
+
+    useEffect(() =>{
+        if(inView)
+          setPageIndex(prevPageIndex => Math.min(prevPageIndex + 1, totalNumberOfPages - 1));
+    }, [inView])
+
+
+    useEffect(() => {
+        loadMore();
+    }, [pageIndex]);
+
+    const loadMore = async () => {
+        try {
+          const headers = {
+            'Content-Type': 'application/json',
+            Authorization: auth!,
+            mode: 'cors',
+          };
+          // TODO set this variables by a date picker 
+          // if attributes is null it means no date filter is applied
+          const day = null;
+          const month = null; 
+          const year = null;
+    
+          const postFilterDTO = {
+            pageNumber: pageIndex,
+            pageSize: pageSize,
+            day: day,
+            month: month,
+            year: year
+          };
+      
+          const response = await fetch(BASE_BACKEND_URL + props.feedType, {
+            method: 'POST',
+            body: JSON.stringify(postFilterDTO),
+            headers,
+          });
+      
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+      
+          const newData = await response.json();
+          setTotalNumberOfPages(newData.totalPages);
+          setPosts((prevPosts) => [...(prevPosts ?? []), ...newData.content]);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
 
     return (
         <Box className={styles.feed} width={props.width}>
             {posts.map((post: any) => (
                 <Post key={post.id} post={post} />
             ))}
-            
+            <Box>
+                <div className={styles.load}/>
+                <div ref = {ref}/>
+            </Box>
         </Box>
     )
 }
--- front_end/src/app/feed/page.tsx
+++ front_end/src/app/feed/page.tsx
@@ -1,31 +1,13 @@
 'use client'
 import styles from './page.module.css'
 import React, { useState, useEffect } from 'react';
-import { useInView } from "react-intersection-observer"
 import { Box } from '@mui/system';
 import Posts from '@/app/components/posts/page';
 import SideBar from '@/app/components/sidebar/page';
-import PostDTO from '../models/dtos/PostDTO';
-import { BASE_BACKEND_URL, DIARY_ENDPOINT, EXPLORE_ENDPOINT, FEED_ENDPOINT } from '../constants/apiConstants';
+import { DIARY_ENDPOINT, EXPLORE_ENDPOINT, FEED_ENDPOINT } from '../constants/apiConstants';
 
 const Feed = () => {
   const [selectedFeedIndex, setSelectedFeedIndex] = useState(0);
-  const [pageIndex, setPageIndex] = useState(0);
-  const [pageSize, setPageSize] = useState(5);
-  const [feedData, setFeedData] = useState<null | PostDTO[]>(null);
-  const [totalNumberOfPages, setTotalNumberOfPages] = useState(10);
-  const [auth, setAuth] = useState<string | null>(null);
-  const {ref, inView } = useInView();
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-
-
-  const handleDateChange = (newDate:Date) => {
-    setSelectedDate(newDate);
-  };
-
-  useEffect(() => {
-    setAuth(localStorage.getItem('Authorization'));
-  }, []); // Empty dependency array to run the effect only once during mount
 
   const endpointMapping: Record<number, string> = {
     0: FEED_ENDPOINT,
@@ -39,80 +21,12 @@ const Feed = () => {
   const handleChange = (newSelectedPageIndex: number) => {
     setSelectedFeedIndex(newSelectedPageIndex);
   };
-
-  useEffect(() => {
-    setPageIndex(0)
-    setFeedData(null)
-    loadMore()
-  }, [selectedFeedIndex, selectedDate]);
-
-  useEffect(() =>{
-    if(inView){
-      console.log("scrolled to the end")
-      setPageIndex(prevPageIndex => Math.min(prevPageIndex + 1, totalNumberOfPages - 1));
-    }
-  }, [inView])
-
-  useEffect(() => {
-    loadMore();
-  }, [pageIndex]);
-
-  const loadMore = async () => {
-    try {
-      const endpoint = endpointMapping[selectedFeedIndex];
-      if (!endpoint) {
-        console.error('Invalid selectedFeedIndex:', selectedFeedIndex);
-        return;
-      }
   
-      const headers = {
-        'Content-Type': 'application/json',
-        Authorization: auth!,
-        mode: 'cors',
-      };
-      // TODO set this variables by a date picker 
-      // if attributes is null it means no date filter is applied
-      const day = null;
-      const month = null; 
-      const year = null;
-
-      const postFilterDTO = {
-        pageNumber: pageIndex,
-        pageSize: pageSize,
-        day: day,
-        month: month,
-        year: year
-      };
-  
-      const response = await fetch(BASE_BACKEND_URL + endpoint, {
-        method: 'POST',
-        body: JSON.stringify(postFilterDTO),
-        headers,
-      });
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-      const newData = await response.json();
-      console.log(newData);
-      setTotalNumberOfPages(newData.totalPages);
-      setFeedData((prevData) => [...(prevData ?? []), ...newData.content]);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-  
-
   return (
     <div>
       <Box className={styles.container}>
         <SideBar width={'25%'} onChange={handleChange} />
-        <Posts width={'75%'} data={feedData} />
-      </Box>
-      <Box>
-        <div className={styles.load}/>
-        <div ref = {ref}/>
+        <Posts width={'75%'} feedType={endpointMapping[selectedFeedIndex]}  />
       </Box>
     </div>
   );
