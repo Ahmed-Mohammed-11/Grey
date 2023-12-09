564fc58eb11e325ed0b05e6de5d767b85cca9ef0
OmarTammam25 <omartammam25@gmail.com>
2023-12-09 20:50:55 +0200

    Add comments for the combiner class

--- back_end/src/main/java/com/software/grey/recommendationsystem/CombinerImp.java
+++ back_end/src/main/java/com/software/grey/recommendationsystem/CombinerImp.java
@@ -12,10 +12,14 @@ import java.util.TreeSet;
 @Component
 public class CombinerImp implements Combiner{
 
+    /**
+     * @param posts A list of recommneded posts, retrieved by any recommendation heuristic
+     * @return A list of posts sorted by decreasing order of post time
+     */
     @Override
     public List<Post> combine(List<List<Post>> posts) {
         Set<Post> set = new TreeSet<>();
-        // merge sorted lists into one
+        // merge sorted lists into one using a set
         for(List<Post> postList: posts)
             for (Post post: postList)
                 set.add(post);
