0006d2ac212746cdb2d21e0dce28a39634220b9d
josephShokry <josephshokry9@gmail.com>
2023-12-26 13:05:34 +0200

    add saved time to the saved post entity and filter and sort the saved posts by this time

--- back_end/src/main/java/com/software/grey/repositories/SavedPostRepository.java
+++ back_end/src/main/java/com/software/grey/repositories/SavedPostRepository.java
@@ -15,16 +15,17 @@ import java.util.List;
 @Repository
 public interface SavedPostRepository extends JpaRepository<SavedPost, SavedPostId> {
     @Query(value = """
-        SELECT sp.user_id, sp.post_id
+        SELECT sp.user_id, sp.post_id, sp.post_saved_time
         FROM saved_post sp
         JOIN post p ON p.id = sp.post_id
         JOIN user u ON u.id = sp.user_id
         JOIN post_feelings pf ON pf.post_id = p.id
-        WHERE (pf.feeling IN (:feelings))
-        AND (:day IS NULL OR DAY(p.post_time) = :day)
-        AND (:month IS NULL OR MONTH(p.post_time) = :month)
-        AND (:year IS NULL OR YEAR(p.post_time) = :year)
-        ORDER BY p.post_time DESC
+        WHERE u.username = :username
+        AND (pf.feeling IN (:feelings))
+        AND (:day IS NULL OR DAY(sp.post_saved_time) = :day)
+        AND (:month IS NULL OR MONTH(sp.post_saved_time) = :month)
+        AND (:year IS NULL OR YEAR(sp.post_saved_time) = :year)
+        ORDER BY sp.post_saved_time DESC
         """,
             countQuery = """
         SELECT count(sp.user_id)
@@ -32,13 +33,16 @@ public interface SavedPostRepository extends JpaRepository<SavedPost, SavedPostI
         JOIN post p ON p.id = sp.post_id
         JOIN user u ON u.id = sp.user_id
         JOIN post_feelings pf ON pf.post_id = p.id
-        WHERE (pf.feeling IN (:feelings))
-        AND (:day IS NULL OR DAY(p.post_time) = :day)
-        AND (:month IS NULL OR MONTH(p.post_time) = :month)
-        AND (:year IS NULL OR YEAR(p.post_time) = :year)
+        WHERE u.username = :username
+        AND (pf.feeling IN (:feelings))
+        AND (:day IS NULL OR DAY(sp.post_saved_time) = :day)
+        AND (:month IS NULL OR MONTH(sp.post_saved_time) = :month)
+        AND (:year IS NULL OR YEAR(sp.post_saved_time) = :year)
+        ORDER BY sp.post_saved_time DESC
         """, nativeQuery = true,
             countProjection = "com.software.grey.models.entities.Post")
     Page<SavedPost> findSavedPostsByUsernameAndDayMonthYear(
+            @Param("username") String username,
             @Param("feelings") List<String> feelings,
             @Param("day") Integer day,
             @Param("month") Integer month,
--- back_end/src/main/java/com/software/grey/services/SavedPostService.java
+++ back_end/src/main/java/com/software/grey/services/SavedPostService.java
@@ -3,9 +3,6 @@ package com.software.grey.services;
 import com.software.grey.SavedPostEnum;
 import com.software.grey.models.dtos.PostDTO;
 import com.software.grey.models.dtos.PostFilterDTO;
-import com.software.grey.models.entities.Post;
-import com.software.grey.models.entities.SavedPost;
-import com.software.grey.models.entities.SavedPostId;
 import org.springframework.data.domain.Page;
 
 public interface SavedPostService {
